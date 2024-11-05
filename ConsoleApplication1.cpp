432414c303dbf4f6c0d1607ac6d3318cd27411b6
DeepAmnesia <81936004+DeepAmnesia@users.noreply.github.com>
2024-11-05 12:27:58 +0300

    Update ConsoleApplication1.cpp
    rework

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,149 +1,109 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <string>
 #include <fstream>
 
-typedef std::pair<std::string, std::string> WordPair;
+typedef std::pair<std::string, std::string> word_pair_t;
 
-class WordsChanger {
-public:
-    WordsChanger(std::vector<std::string>& StringsVector, std::vector<WordPair>& Dict) : StringsToStransform(StringsVector), ReserveDictionary(Dict) { };
+void extract_words(const std::string& str, std::vector<std::string>& target) {
+    if (str.empty()) return;
 
-    void Change() {
-        if (StringsToStransform.empty() || ReserveDictionary.empty()) return;
+    int start = 0, end = 0;
 
-        for (auto& String : StringsToStransform) {
-            ChangeAllWords(String);
-        }
+    while ((start = str.find_first_not_of(' ', end)) != std::string::npos) {
+        end = str.find(' ', start);
+        target.push_back(str.substr(start, end - start));
+    }
+}
+
+void change_current_word(std::string& word, std::vector<word_pair_t>& reserve_dictionary) {
+    if (word.empty()) return;
+
+    auto need_string = [](const word_pair_t& list, const std::string& target) {
+        return strcmp(list.first.c_str(), target.c_str()) == 0 ? list.second : target;
+    };
+
+    for (auto& element : reserve_dictionary)
+        word = need_string(element, word);
+}
+
+std::string compile_string(std::vector<std::string>& list_of_words) {
+    std::string resilt = "";
+
+    for (auto& str : list_of_words) {
+        resilt.append(str);
+        if (strcmp(str.c_str(), list_of_words.at(list_of_words.size() - 1).c_str()) != 0)
+            resilt.append(" ");
     }
 
-    void PrintStrings() {
-        std::cout << "[ ~ ] Current strings state:" << std::endl;
+    return resilt;
+}
 
-        for (auto& String : StringsToStransform) {
-            std::cout << String << std::endl;
-        }
+std::string& change_all_words(std::string& str, std::vector<word_pair_t>& reserve_dictionary) {
+    if (!str.empty()) {
+
+        std::vector<std::string> words = { };
+        extract_words(str, words);
+
+        for (auto& word : words)
+            change_current_word(word, reserve_dictionary);
+
+        str = compile_string(words);
+    }
+}
+
+void read_dictionary(const std::string& path, std::vector<word_pair_t>& target) {
+    std::ifstream file(path);
+
+    if (!file.is_open()) return;
+
+    std::string cache_line;
+
+    while (getline(file, cache_line)) {
+        size_t pos = cache_line.find("\\t");
+        word_pair_t pair = { cache_line.substr(0, pos), cache_line.substr(pos + 2) };
+        target.push_back(pair);
     }
 
-    void PrintDictionary() {
-        std::cout << "[ ~ ] Current dictionary:" << std::endl;
+    file.close();
+}
 
-        for (auto& String : ReserveDictionary) {
-            std::cout << "[" << String.first << " | " << String.second << "]" << std::endl;
-        }
-    }
-private:
-    void ExtractWords(const std::string& String, std::vector<std::string>& Target) {
-        if (String.empty()) return;
+void read_strings(const std::string& end, std::vector<word_pair_t>& reserve_dictionary) {
+    std::string cache_string = "";
 
-        //Можно было через stringstream т.к. он принимает строки разделенные пробелом
-        //но решил ограничиться библиотеками и сделать используя только string
-
-        int Start = 0, End = 0;
-       
-        while ((Start = String.find_first_not_of(' ', End)) != std::string::npos) {
-            End = String.find(' ', Start);
-            Target.push_back(String.substr(Start, End - Start));
-        }
-    }
-
-    void ChangeCurrentWord(std::string& Word) { 
-        if (Word.empty()) return;
-
-        auto NeededString = [](const std::pair<std::string, std::string>& List, const std::string& Target) {
-            return strcmp(List.first.c_str(), Target.c_str()) == 0 ? List.second : Target;
-        };
-
-        for (auto& Element : ReserveDictionary)
-            Word = NeededString(Element, Word);
-    }
-
-    std::string CompileString(std::vector<std::string>& ListOfWords) {
-        std::string Result = "";
-       
-        for (auto& String : ListOfWords) {
-            Result.append(String);
-            if (strcmp(String.c_str(), ListOfWords.at(ListOfWords.size() - 1).c_str()) != 0)
-                Result.append(" ");
-        }
-
-        return Result;
-    }
-
-    std::string& ChangeAllWords(std::string& String) {
-        if (!String.empty()) {
-          
-            std::vector<std::string> Words = { };
-            ExtractWords(String, Words);
-         
-            for (auto& Word : Words) 
-                ChangeCurrentWord(Word);
-
-            String = CompileString(Words);
-        }
-    }
-
-private:
-    std::vector<std::string> StringsToStransform = { };
-    std::vector<WordPair> ReserveDictionary = { };
-};
-
-class Reader {
-public:
-    void ReadDictionary(const std::string& Path, std::vector<WordPair>& Target) {
-        std::ifstream File(Path);
-
-        if (!File.is_open()) return;
-        
-        std::string CacheLine;
-
-        while (getline(File, CacheLine)) {
-            size_t Position = CacheLine.find("\\t");
-            WordPair Pair = { CacheLine.substr(0, Position), CacheLine.substr(Position + 2) };
-            Target.push_back(Pair);
-        }
-
-        File.close();
-    }
-
-    void ReadStrings(const std::string& End, std::vector<std::string>& StringsToStransform) {
-        std::string CacheString = "";
-
-        while (std::getline(std::cin, CacheString)) {
-            if (strcmp(CacheString.c_str(), End.c_str()) != 0) {
-                if (!CacheString.empty()) {
-                    StringsToStransform.push_back(CacheString);
-                }
+    while (std::getline(std::cin, cache_string)) {
+        if (strcmp(cache_string.c_str(), end.c_str()) != 0) {
+            if (!cache_string.empty()) {
+                change_all_words(cache_string, reserve_dictionary);
+                std::cout << cache_string << std::endl;
             }
-            else
-                break;  
         }
+        else break;
     }
-};
+}
+
+void print_dictionary(std::vector<word_pair_t>& reserve_dictionary) {
+    std::cout << "[ ~ ] Current dictionary:" << std::endl;
+
+    for (auto& str : reserve_dictionary) {
+        std::cout << "[" << str.first << " | " << str.second << "]" << std::endl;
+    }
+}
 
 int main() {
-    Reader ReaderObject;
-
-    std::vector<WordPair> WordPairs = { };
-    ReaderObject.ReadDictionary("words.txt", WordPairs);
+    std::vector<word_pair_t> word_pairs = { };
+    read_dictionary("words.txt", word_pairs);
 
     std::cout << "[ + ] Dictionary read sucessfully" << std::endl;
+
+    print_dictionary(word_pairs);
+
     std::cout << "[ ~ ] Input strings: " << std::endl;
-
-    std::vector<std::string> StringsToStransform = { };
-    ReaderObject.ReadStrings("\\n", StringsToStransform);
-
-    WordsChanger ChangerObject = WordsChanger(StringsToStransform, WordPairs);
-    ChangerObject.PrintStrings();
-
-    std::cout << "[ ~ ] Changing..." << std::endl;
-
-    ChangerObject.Change();
-    ChangerObject.PrintDictionary();
-    ChangerObject.PrintStrings();
+  
+    read_strings("\\n", word_pairs);
 
     std::cout << "[ + ] Done" << std::endl;
     system("pause");
 
     return 0;
-}
+} 
